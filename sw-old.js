a459a8bbc9b8c56f15bc4e48a64d719690d93a8a
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-04-05 18:45:56 +0500

    Add files via upload

---
+++ sw-old.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => caches.delete(cache))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response("", { status: 404 });
+    })
+  );
+});
